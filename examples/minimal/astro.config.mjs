dc4615ead564427c3109c636b1dffe8cbd85742c
Nate Moore <nate@astro.build>
2022-10-25 16:07:32 -0500

    wip: hybrid output

--- examples/minimal/astro.config.mjs
+++ examples/minimal/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-	srcDir: '.',
-	root: '.',
+	output: 'hybrid',
+	adapter: node({
+		mode: 'standalone'
+	})
 });
